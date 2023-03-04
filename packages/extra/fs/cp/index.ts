66bfffd6cc3e46af3cb6a13add1fc76d44cb08fc
saqqdy <saqqdy@qq.com>
2023-03-04 20:26:38 +0800

    add cp cpSync

---
+++ packages/extra/fs/cp/index.ts
@@ -0,0 +1,80 @@
+import { isAbsolute, join } from 'path'
+import {
+	type PathLike,
+	type RmDirOptions,
+	existsSync,
+	lstatSync,
+	promises,
+	readdirSync,
+	rmdirSync,
+	unlinkSync
+} from 'fs'
+
+export interface Options extends RmDirOptions {
+	force?: boolean
+}
+
+/**
+ * remove file
+ *
+ * @param paths - path or file, support array
+ */
+export async function cp(paths: PathLike | PathLike[], options?: Options): Promise<void> {
+	if (!(paths instanceof Array)) paths = ([] as PathLike[]).concat(paths)
+
+	for (let path of paths) {
+		typeof path === 'string' && !isAbsolute(path) && (path = join(process.cwd(), path))
+		if (!existsSync(path)) {
+			console.info(`[NOT_EXIST]: "${path}" does not exist`)
+			continue
+		}
+		// get stat
+		const stat = await promises.lstat(path)
+		if (stat.isFile()) {
+			await promises.unlink(path)
+			continue
+		}
+
+		// get files
+		const files = await promises.readdir(path)
+		await cp(files.map(name => (typeof path === 'string' ? join(path, name) : name)))
+
+		// remove dir
+		await promises.rmdir(path, options)
+	}
+}
+
+/**
+ * remove file sync
+ *
+ * @param paths - path or file, support array
+ */
+export function cpSync(paths: PathLike | PathLike[], options?: Options): void {
+	if (!(paths instanceof Array)) paths = ([] as PathLike[]).concat(paths)
+
+	for (let path of paths) {
+		typeof path === 'string' && !isAbsolute(path) && (path = join(process.cwd(), path))
+		if (!existsSync(path)) {
+			console.info(`[NOT_EXIST]: "${path}" does not exist`)
+			continue
+		}
+		// get stat
+		const stat = lstatSync(path)
+		if (stat.isFile()) {
+			unlinkSync(path)
+			continue
+		}
+
+		// get files
+		const files = readdirSync(path)
+		cpSync(files.map(name => (typeof path === 'string' ? join(path, name) : name)))
+
+		// remove dir
+		rmdirSync(path, options)
+	}
+}
+
+export default {
+	cp,
+	cpSync
+}
